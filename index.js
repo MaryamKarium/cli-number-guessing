eb823c87cd7fd25d461995059bc5bf3aae739c95
MaryamKarium <mariumkareem18@gmail.com>
2024-05-16 03:10:28 -0700

    Push number guessing game to github

---
+++ index.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Guess a number between 1-10:",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! you guessed right number");
+}
+else {
+    console.log("You guessed wrong number");
+}
